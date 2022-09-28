5a3900bba6ff30c83b6ae4cd91db2fda1c92fd99
Jean Damascene CYIFUZO <jeancyifuzodamas@gmail.com>
2022-09-28 23:31:50 +0200

    ft-project set up

---
+++ src/api/index.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const BASE_URL = "http://localhost:5000/api/v1/";
+// const BASE_URL= "https://myechelon-app.herokuapp.com/api/v1/"
+
+export const axiosRequest = axios.create({
+  baseURL: BASE_URL,
+});
+
+export default axios.create({
+  baseURL: BASE_URL,
+});
